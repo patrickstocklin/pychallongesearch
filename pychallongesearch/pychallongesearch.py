81f86e51298a91cd8ae6180473e18452195a2194
patrickstocklin <patrickstocklin@gmail.com>
2018-09-19 23:37:55 -0400

    quick formatting

--- pychallongesearch/pychallongesearch.py
+++ pychallongesearch/pychallongesearch.py
@@ -18,7 +18,7 @@ import logging
 #    Author: Patrick C. Stocklin
 #	
 #        Python Wrapper to handle the creation, insertion, updating, and searching of player, tournament, and match stats
-#    within a self-defined, self-contained Elasticsearch instance. Useful for analysis across an aggregated collection of
+#    within a self-defined, self-contained Elasticsearch cluster. Useful for analysis across an aggregated collection of
 #    double-elimination Challonge Brackets.
 #
 ############################################################################################################################
