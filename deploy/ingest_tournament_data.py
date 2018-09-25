bd0ab3dd788e13e40457440adf3a52afa9eaab20
patrickstocklin <patrickstocklin@gmail.com>
2018-09-24 23:23:30 -0400

    testing

--- deploy/ingest_tournament_data.py
+++ deploy/ingest_tournament_data.py
@@ -11,7 +11,11 @@ def ingest_tournament_data():
   print "Ingesting Tournament Data"
   pcsearch = pcs.PyChallongeSearch("127.0.0.1","9200")
   pcsearch.indices.create_elasticsearch_indices()
-  pcsearch.brackets.ingest_bracket();
+  #pcsearch.brackets.ingest_bracket();
+  
+  #some quick way to do the listDirs testing, remove later
+
+
   print "Completed Inserting Tournament Data"
 
 if __name__ == '__main__':
