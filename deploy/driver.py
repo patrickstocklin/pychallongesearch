ba984cb3186a2a656c6d84eb08ade66ce80279ed
patrickstocklin <patrickstocklin@gmail.com>
2018-09-25 21:42:19 -0400

    testing

--- deploy/driver.py
+++ deploy/driver.py
@@ -10,7 +10,7 @@ CONSTANTS
 def driver():
   print "Running Driver"
   pcsearch = pcs.PyChallongeSearch("127.0.0.1","9200")
-  pcsearch.indices.create_elasticsearch_indices()
+  # pcsearch.indices.create_elasticsearch_indices()
   pcsearch.brackets.ingest_series("/home/pat/smashdb/data/tournaments/MNM")
   print "Exiting Driver"
 
