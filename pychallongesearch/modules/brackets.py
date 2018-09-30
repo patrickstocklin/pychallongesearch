b3d523881a8485ab7af3ce5f0f785bb0de3277f0
patrickstocklin <patrickstocklin@gmail.com>
2018-09-29 23:55:03 -0400

    cleanup

--- pychallongesearch/modules/brackets.py
+++ pychallongesearch/modules/brackets.py
@@ -19,36 +19,6 @@ class brackets(object):
     self.elasticsearch_client = parent.elasticsearch_client
     self.logger = parent.logger
 
-  #Given a directory, look at all of the tournaments, matches, and player files
-  #M  Load contents and strip prepending tourney ID (name)
-  #O  Merge sort 3 dirs according to ID ( order is 1, 10, 100, 101, 102 ... 109, 11, 110, 111, 112 ...)
-  #V
-  #E  def ingest(3 hashmaps of sorted json objects):
-  #  if len of 3 items not equal, something is missing:
-  #T    #set bool to warn
-  #O
-  #    for (list item in list of items):
-  #C      #if item.ID != the others, continue without inserting and log it
-  #F      readtourneyfromjson
-  #U      readmatchesfromjson
-  #s      readparticipantsfromjson
-  #      ingest bracket()
-  #Insert with the super special secret sauce algorithm (may require more work on query commands, spec. update and insert)
-
-  # For tournamentDir under ~/data/tournaments:
-      # tA, pA, mA = fileutils.readBrack
-
-    #  tournamentsarr, participantsarr, matchesarr = self.parent.challongefileutils.readBracketFromDirectory(String directory):
-      #          find all jsons, insert 
-      #          []
-      #
-      #
-      #
-      #
-    # MOVE THESE
-    # MOVE THESE
-    # MOVE THESE
-
   def ingest_series(self, targetDirectory):
   	sortedMatchesDict, sortedParticipantsDict, sortedTournamentsDict = self.parent.challongefileutils.createSortedDictsOfFilesInTournamentSeriesDirectory(targetDirectory)
 
@@ -60,9 +30,6 @@ class brackets(object):
 				print sortedTournamentsDict[i][1]
 				# self.ingest_bracket(sortedMatchesDict[i][1], sortedParticipantsDict[i][1], sortedTournamentsDict[i][1])
 
-  # Given: 3 filepaths to bracket's jsons
-  #
-  #
   def ingest_bracket(self, targetMatchesJsonFile, targetParticipantsJsonFile, targetTournamentJsonFile):
     self.logger.info("ingesting bracket")
 
